e29a6d0b198974ab533174a950ea71b9e02392cf
mjknowles <mjknowles23@gmail.com>
2016-12-26 12:13:01 -0600

    Built basic project structure

---
+++ src/bl/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public static string Method1()
+        {    
+            return "Hello, world!"
+        }
+    }
+}
